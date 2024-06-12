f00b6f5319b02a245899a93cd845c6538f97e077
Usama-Khan99 <65035298+Usama-Khan99@users.noreply.github.com>
2024-06-12 20:06:11 +0500

    Distructuring of array

--- main.js
+++ main.js
@@ -1,51 +1,27 @@
 // Revision of for loop 
 // Assignment (While loop)
-
 // while loop
-
-let i =0;
-
+var i = 0;
 // while (i > 0){
 //     console.log('Inside while loop');
-    
 // }
-
 // do while loop
 // jbb koi kam bar bar karna ho lkn first time uska krna laxmi ho tou do while loop use karygy
-
 do {
     console.log("Inside do while");
-    }
-while (i > 0);
-
-
+} while (i > 0);
 // for or while loop pehly condition check karty h phr kam karty h
-
 //  do while pehly kam karta h phr condition check karta h
-
-
 // 5 star ka pattern banana h using do while loop // Assignment (nested do whhile loop use hoga)
-
 // Assignment For-of Loop 
-
-
-
 // Distructuring of array 
-
-let grocery : string[] = ['Atta', 'Cheeni','CHawal','Basin'];
-
+var grocery = ['Atta', 'Cheeni', 'CHawal', 'Basin'];
 // agr hum chahty h k is list m sy koi item nikal k us ko seperate deal kary
-
 // let item1 = grocery[0]; // agr chota array h tou ye method use kar sakty h
-
 //lkn agr hamary paas array m 100 elements h or hum chahty h k shuru k 3 elements ko alag variable m deal kary
-
-let [item1, item2, item3] = grocery;
-
+// array ko distructure karny k bad uski list m wo elements baqi nh rehty
+var item1 = grocery[0], item2 = grocery[1], item3 = grocery[2];
 console.log(item1);
 console.log(item2);
 console.log(item3);
-
 console.log(grocery);
-
-
