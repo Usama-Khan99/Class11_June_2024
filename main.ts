f6f06a589cb78a2d4e92214e6a30c19304331232
Usama-Khan99 <65035298+Usama-Khan99@users.noreply.github.com>
2024-06-12 19:47:22 +0500

    do while loop

---
+++ main.ts
@@ -0,0 +1,23 @@
+// Revision of for loop 
+// Assignment (While loop)
+
+// while loop
+
+let i =0;
+
+// while (i > 0){
+//     console.log('Inside while loop');
+    
+// }
+
+// do while loop
+
+do {
+    console.log("Inside do while");
+    }
+while (i > 0);
+
+
+// for or while loop pehly condition check karty h phr kam karty h
+
+//  do while pehly kam karta h phr condition check karta h
